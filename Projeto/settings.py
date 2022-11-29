6a7f3ada8833fca53a551149b942b9a8d1bae007
emerss001 <emersonnevess80@gmail.com>
2022-11-28 23:26:22 -0300

    redefinir senha esquecida - parte 1

--- Projeto/settings.py
+++ Projeto/settings.py
@@ -131,3 +131,12 @@ MESSAGE_TAGS = {
     constants.ERROR: 'alert-error',
     constants.SUCCESS: 'alert-success'
 }
+
+# Configurações de envio do e-mail
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "suporte.class.school@gmail.com"
+EMAIL_HOST_PASSWORD = "qlmztjfffrvsqyyj"
--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', views.login, name="login"),
     path('registrar/', views.registrar, name="registrar"),
     path('logout/', views.logout, name="logout"),
+
+    path('redefinir-senha/', auth_views.PasswordResetView.as_view(), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
 
 
