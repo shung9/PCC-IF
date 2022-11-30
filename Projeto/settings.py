6046c30408325feb0a8b9231c9cfd149bf7d473b
emerss001 <emersonnevess80@gmail.com>
2022-11-29 22:45:50 -0300

    redefinir senha esquecida - completo

--- Projeto/settings.py
+++ Projeto/settings.py
@@ -138,5 +138,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "suporte.class.school@gmail.com"
-EMAIL_HOST_PASSWORD = "qlmztjfffrvsqyyj"
+EMAIL_HOST_USER = "suport.class.school@gmail.com"
+EMAIL_HOST_PASSWORD = "zezqrbzbxxlyfqea"
--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('logout/', views.logout, name="logout"),
 
     path('redefinir-senha/', auth_views.PasswordResetView.as_view(), name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path('redefinição-enviada/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('redefinir/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('redefinição-completa/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
 
 
